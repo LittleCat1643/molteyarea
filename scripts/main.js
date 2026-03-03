39555fb45847b532e531c9954607480f8edb196c
littlecat1643 <102461102+LittleCat1643@users.noreply.github.com>
2026-03-03 21:31:42 +0400

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,13 @@
 const colors = [['ff4d6d', 'ffb3c1'], ['0582ca', '00a6fb'], ['2a9134', '5bba6f'], ['ffba08', 'ffe169'], ['7b2cbf', 'c77dff'], ['e85d04', 'faa307']];
 const radius = [[0, 0], [1, 0], [1, 1], [0, 1], [-1, 0], [-1, -1], [0, -1], [1, -1], [-1, 1]];
 
+const leaderboardMax = 3;
+
+const enFirstUsernamesPart = ['Good', 'Bad', 'Happy', 'Sad', 'Big', 'Small', 'Young', 'Old', 'Beautiful', 'Ugly', 'Nice', 'Mean', 'Kind', 'Cruel', 'Clever', 'Stupid', 'Busy', 'Lazy', 'Quiet', 'Noisy', 'Hot', 'Cold', 'Hungry', 'Full', 'Tired', 'Rested', 'Strong', 'Weak', 'Brave', 'Scared', 'Rich', 'Poor', 'Important', 'Unimportant', 'Easy', 'Difficult', 'Interesting', 'Boring', 'Exciting', 'Calming', 'Funny', 'Serious', 'Angry', 'Friendly', 'Polite', 'Rude', 'Clean', 'Dirty', 'New', 'Old', 'Modern', 'Traditional', 'Comfortable', 'Uncomfortable', 'Safe', 'Dangerous', 'Healthy', 'Sick', 'Fast', 'Slow', 'Long', 'Short', 'Deep', 'Shallow', 'High', 'Low', 'Wide', 'Narrow', 'Thick', 'Thin', 'Heavy', 'Light', 'Soft', 'Hard', 'Smooth', 'Rough'];
+const enSecondUsernamesPart = ['Time', 'Person', 'Year', 'Way', 'Day', 'Thing', 'Man', 'World', 'Life', 'Hand', 'Part', 'Child', 'Eye', 'Woman', 'Place', 'Work', 'Week', 'Case', 'Point', 'Government', 'Company', 'Number', 'Group', 'Problem', 'Fact', 'Side', 'Member', 'Family', 'System', 'Area', 'Year', 'Community', 'Name', 'Thing', 'Country', 'House', 'Service', 'Friend', 'Story', 'Customer', 'Job', 'School', 'City', 'State', 'Plant', 'Resource', 'Money', 'Air', 'Health', 'Structure', 'Car', 'Product', 'Information', 'Food', 'Safety', 'Water', 'Room', 'Level', 'Book', 'Top', 'Teacher', 'Line', 'Order', 'Word', 'Question', 'Door', 'Answer', 'Key', 'Phone', 'Result', 'Week', 'Girl', 'Boy', 'Minute', 'Corner', 'Face', 'Tax', 'Hour', 'Plan', 'Brand', 'Rule', 'Mind', 'Shape', 'Goal', 'Step', 'Weight', 'Pattern', 'Price', 'View', 'Ball', 'Month', 'Paper', 'Sign', 'Glass', 'Kind', 'Space', 'Sun', 'Music'];
+const ruFirstUsernamesPart = ['Александр', 'Дмитрий', 'Максим', 'Иван', 'Михаил', 'Владимир', 'Сергей', 'Андрей', 'Николай', 'Виктор', 'Олег', 'Юрий', 'Артем', 'Антон', 'Павел', 'Игорь', 'Роман', 'Евгений', 'Анатолий', 'Егор', 'Никита', 'Алексей', 'Валерий', 'Семён', 'Кирилл', 'Денис', 'Вячеслав', 'Тимофей', 'Владислав', 'Степан', 'Леонид', 'Борис', 'Григорий', 'Виктор', 'Эдуард', 'Семён', 'Василий', 'Виталий', 'Александр', 'Руслан', 'Тимур', 'Даниил', 'Матвей', 'Константин', 'Виктор', 'Мирон', 'Илья', 'Родион', 'Глеб', 'Лев', 'Ярослав', 'Марк', 'Егор', 'Кирилл'];
+const ruSecondUsernamesPart = ['Иванов', 'Смирнов', 'Кузнецов', 'Попов', 'Васильев', 'Петров', 'Соколов', 'Михайлов', 'Новиков', 'Фёдоров', 'Морозов', 'Волков', 'Алексеев', 'Лебедев', 'Семёнов', 'Егоров', 'Павлов', 'Батурин', 'Степанов', 'Николаев', 'Орлов', 'Андреев', 'Макаров', 'Никитин', 'Захаров', 'Зайцев', 'Соловьёв', 'Васильев', 'Быков', 'Воробьёв', 'Гусев', 'Фёдоров', 'Чушкин', 'Молотов', 'Миронов', 'Белов', 'Комаров', 'Григорьев', 'Степанов', 'Коняхин', 'Киселёв', 'Сергеев', 'Кириллов', 'Воронов', 'Блинников', 'Макаров', 'Анисимов', 'Логинов', 'Шутов', 'Романов', 'Игнатьев', 'Жданов', 'Филиппов'];
+
 let width = 50;
 let height = 50;
 let strength = 50;
@@ -10,12 +17,14 @@ const cellStandard = [strength, 0, false, false];
 
 let map = Array(height).fill().map(() => Array(width).fill().map(() => [strength, 0, false, false]));
 
-let player = { id: 1, x: 25, y: 25, cellMin: 1, cellMax: 99, captured: [], canFlag: false, flagCount: 1, flagsAvailable: 0, kills: 0 }
+let player = { id: 1, nickname: 'Игрок', x: 25, y: 25, cellMin: 1, cellMax: 99, captured: [], canFlag: false, flagCount: 1, flagsAvailable: 0, kills: 0 }
 let zoom = 4;
 
 const game = document.querySelector('.field');
 const container = document.querySelector('.game');
 const layout = document.querySelector('.layout');
+
+const leaderboard = document.querySelector('.header > .top > .left > .leaderboard > .content');
 
 const joystick = [
     document.querySelector('.header > .bottom > .left > .joystick > .top > .button:nth-child(1) > button'),
@@ -76,6 +85,10 @@ function generateMap() {
             game.appendChild(row);
         });
     });
+}
+
+function checkPos(x, y) {
+    return !players.some(player => player.x == x && player.y == y);
 }
 
 function generatePlayer() {
@@ -188,7 +201,7 @@ function createBot() {
     const validCells = findValidCells();
     const cell = validCells[Math.floor(Math.random() * validCells.length)];
     
-    const bot = { id: players.length + 1, x: cell.x, y: cell.y, cellMin: 1, cellMax: 99, captured: [], canFlag: false, flagCount: 1, flagsAvailable: 0, kills: 0 }
+    const bot = { id: players.length + 1, nickname: generateBotUsername(), x: cell.x, y: cell.y, cellMin: 1, cellMax: 99, captured: [], canFlag: false, flagCount: 1, flagsAvailable: 0, kills: 0 }
 
     players.push(bot);
 
@@ -207,6 +220,18 @@ function generateBot(bot) {
     botElement.style.background = `#${colors[bot.id - 1][0]}`;
 
     cell.appendChild(botElement);
+}
+
+function generateBotUsername() {
+    const random = Math.floor(Math.random() * 3);
+
+    if (random == 0) {
+        return enFirstUsernamesPart[Math.floor(Math.random() * enFirstUsernamesPart.length)] + enSecondUsernamesPart[Math.floor(Math.random() * enSecondUsernamesPart.length)] + Math.floor(Math.random() * 100);
+    } else if (random == 1) {
+        return ruFirstUsernamesPart[Math.floor(Math.random() * ruFirstUsernamesPart.length)] + ' ' + ruSecondUsernamesPart[Math.floor(Math.random() * ruSecondUsernamesPart.length)];
+    } else {
+        return 'Гость ' + (Math.floor(Math.random() * 1000) + 100);
+    }
 }
 
 function generateBotRadius(bot) {
@@ -228,19 +253,52 @@ function movePlayer(direction) {
     const old = { x: player.x, y: player.y };
 
     if (direction == 0 && player.y > 0) {
-        player.y -= 1;
+        if (checkPos(player.x, player.y - 1)) {
+            player.y -= 1;
+        }
     } else if (direction == 1 && player.y < height - 1) {
-        player.y += 1;
+        if (checkPos(player.x, player.y + 1)) {
+            player.y += 1;
+        }
     } else if (direction == 2 && player.x > 0) {
-        player.x -= 1;
+        if (checkPos(player.x - 1, player.y)) {
+            player.x -= 1;
+        }
     } else if (direction == 3 && player.x < width - 1) {
-        player.x += 1;
+        if (checkPos(player.x + 1, player.y)) {
+            player.x += 1;
+        }
     }
 
     generateCount(old.x, old.y);
     generatePlayer();
 
     centerOffset();
+}
+
+function moveBot(bot, direction) {
+    const old = { x: players[bot.id - 1].x, y: players[bot.id - 1].y };
+
+    if (direction == 0 && players[bot.id - 1].y > 0) {
+        if (checkPos(players[bot.id - 1].x, players[bot.id - 1].y - 1)) {
+            players[bot.id - 1].y -= 1;
+        }
+    } else if (direction == 1 && players[bot.id - 1].y < height - 1) {
+        if (checkPos(players[bot.id - 1].x, players[bot.id - 1].y + 1)) {
+            players[bot.id - 1].y += 1;
+        }
+    } else if (direction == 2 && players[bot.id - 1].x > 0) {
+        if (checkPos(players[bot.id - 1].x - 1, players[bot.id - 1].y)) {
+            players[bot.id - 1].x -= 1;
+        }
+    } else if (direction == 3 && players[bot.id - 1].x < width - 1) {
+        if (checkPos(players[bot.id - 1].x + 1, players[bot.id - 1].y)) {
+            players[bot.id - 1].x += 1;
+        }
+    }
+
+    generateCount(old.x, old.y);
+    generateBot(bot);
 }
 
 function playerCapture() {
@@ -258,7 +316,25 @@ function playerCapture() {
 }
 
 function cellCapture(x, y) {
-    player.captured.push([x, y]);
+    let capturedByOther = false;
+    let otherPlayer = null;
+    
+    players.forEach(player => {
+        const cellIndex = player.captured.findIndex(pos => pos[0] == x && pos[1] == y);
+
+        if (cellIndex !== -1 && player.id !== 1) {
+            capturedByOther = true;
+            otherPlayer = player;
+        }
+    });
+    
+    if (capturedByOther && otherPlayer) {
+        otherPlayer.captured = otherPlayer.captured.filter(pos => !(pos[0] === x && pos[1] === y));
+    }
+    
+    if (!player.captured.some(pos => pos[0] == x && pos[1] == y)) {
+        player.captured.push([x, y]);
+    }
 }
 
 function botCellCapture(bot, x, y) {
@@ -290,6 +366,36 @@ function botPassiveCapture(bot) {
                 generateCount(pos[0], pos[1]);
             });
         }, 1000);
+    });
+}
+
+function botActive(bot) {
+    requestAnimationFrame(() => {
+        setInterval(() => {
+            moveBot(bot, Math.floor(Math.random() * 4))
+        }, 1000);
+    });
+}
+
+function generateLeaderboard() {
+    leaderboard.innerHTML = '';
+
+    let counts = [];
+
+    players.forEach(player => {
+        let count = 0;
+
+        player.captured.forEach(cell => {
+            count += map[cell[1]][cell[0]][0]
+        });
+
+        counts.push([player.id, count]);
+    });
+
+    counts = counts.sort((a, b) => b[1] - a[1]).slice(0, leaderboardMax);
+
+    counts.forEach((player, index) => {
+        leaderboard.innerHTML += `<span style="color: #${colors[player[0] - 1][0]};">${index + 1}. ${players[player[0] - 1].nickname} <span>${player[1]}</span></span>`;
     });
 }
 
@@ -380,8 +486,15 @@ function main() {
                 passiveCapture();
             } else {
                 botPassiveCapture(player);
+                botActive(player);
             }
         });
+
+        generateLeaderboard();
+
+        setInterval(() => {
+            generateLeaderboard();
+        }, 2500);
     }, 50);
 }
 
