200d19d28c3314010f491cc2dcbdcc15a60975a3
littlecat1643 <102461102+LittleCat1643@users.noreply.github.com>
2026-03-24 21:14:55 +0400

    Delete scripts directory

--- scripts/info.js
+++
@@ -1,5 +0,0 @@
-const okButton = document.querySelector('.scenes > .info > .ok > button');
-
-okButton.onclick = () => {
-    switchScene(menuScene);
-}
--- scripts/menu.js
+++
@@ -1,9 +0,0 @@
-const nicknameInput = document.querySelector('.scenes > .menu > .ui > .lobby > .top > .nickname > input');
-const skinsButton = document.querySelector('.scenes > .menu > .ui > .lobby > .top > .skins > button');
-const playButton = document.querySelector('.scenes > .menu > .ui > .lobby > .bottom > .play > button');
-const infoButton = document.querySelector('.scenes > .menu > .ui > .lobby > .bottom > .other > .info > button');
-const settingsButton = document.querySelector('.scenes > .menu > .ui > .lobby > .bottom > .other > .settings > button');
-
-infoButton.onclick = () => {
-    switchScene(infoScene);
-}
